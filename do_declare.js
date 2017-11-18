f78f08fa38ed8088d4e7b377c453f8fa88b48564
JulieDaniel <33355156+JulieDaniel@users.noreply.github.com>
2017-11-18 11:39:44 -0800

    i do declare js assignment uploaded

---
+++ do_declare.js
@@ -0,0 +1,24 @@
+var wilson=3;
+var wagner=54;
+var baldwin=89;
+var rawls=34;
+
+var qb="wilson";
+var rb="rawls";
+var wr="baldwin";
+
+var HauschkaISbetter = true;
+var HawksAreAwesome = true;
+
+var nfcCamps=undefined;
+
+console.log("the 4th number: ", rawls);
+console.log("the 1st number: ",  wilson);
+console.log("the 2nd number: ", wagner);
+console.log("the 3rd number: ", baldwin);
+console.log("the 1st string: ", qb);
+console.log("the 2nd string: ", rb);
+console.log("the 3rd string: ", wr);
+console.log("1st boolean: ", HauschkaISbetter);
+console.log("2nd boolean: ", HawksAreAwesome);
+console.log("undefined: ", nfcCamps);
