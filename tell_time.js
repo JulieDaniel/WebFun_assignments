6ae0199f1e484bf585f9ff44793f13fb2b7da105
JulieDaniel <33355156+JulieDaniel@users.noreply.github.com>
2017-11-18 15:31:52 -0800

    js assignment for telling time uploaded

---
+++ tell_time.js
@@ -0,0 +1,56 @@
+var hour=12;
+var min=00;
+var AMorPM="AM";
+
+var preamble;
+var ending;
+
+
+if (min<30)
+{
+    preamble="It's just after";
+}
+else if (min>30)
+{
+    if (hour<12)
+    {
+     hour=hour+1;   
+    }
+    else
+    {
+        hour=1;
+    }
+    
+    preamble="It's almost";
+}
+else
+{   
+    preamble="It's half past";
+}
+
+
+
+if (AMorPM =="AM")
+{
+    ending="in the morning.";
+    if ((hour==12) && (min==0))
+    {
+        preamble= "It's";
+        ending="midnight.";
+    }
+}
+else 
+{   
+    if ((hour>5) && (hour!=12))
+        ending="in the evening.";
+    else
+        ending="in the afternoon.";
+    if ((hour==12) && (min==0))
+        {
+            preamble= "It's";
+            ending="noon.";
+        }
+}
+
+
+console.log(preamble, hour, ending);
